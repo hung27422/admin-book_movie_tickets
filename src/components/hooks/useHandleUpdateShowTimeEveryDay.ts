99ed68535fdb08f57b5cb18eceb678936de5e740
Ho Tan Hung <tanhungho2002@gmail.com>
2025-06-06 01:22:42 +0700

    refactor: rename ID arrays for better clarity in useHandleUpdateShowTimeEveryDay hook

--- src/components/hooks/useHandleUpdateShowTimeEveryDay.ts
+++ src/components/hooks/useHandleUpdateShowTimeEveryDay.ts
@@ -11,7 +11,7 @@ function useHandleUpdateShowTimeEveryDay() {
     if (initialized.current) return; // nếu đã chạy rồi thì không chạy lại
     initialized.current = true;
     const idsDisable = ["68417491a6d3481c9686b7c4"];
-    const ids = [
+    const idsHCM = [
       "684016bd5dac6becaa80414a",
       "68401a125dac6becaa805e64",
       "684019ac5dac6becaa804c35",
@@ -19,7 +19,7 @@ function useHandleUpdateShowTimeEveryDay() {
       "68401aa55dac6becaa806640",
       "68401af45dac6becaa807074",
     ];
-    const ids1 = [
+    const idsHCM1 = [
       "68417c59a6d3481c9686d277",
       "68417c47a6d3481c9686d202",
       "68417c79a6d3481c9686d2e8",
@@ -27,7 +27,7 @@ function useHandleUpdateShowTimeEveryDay() {
       "68417c31a6d3481c9686d191",
       "68417c00a6d3481c9686d0a3",
     ];
-    const ids2 = [
+    const idsHCM2 = [
       "68418293a6d3481c9686e27a",
       "684182a0a6d3481c9686e2e3",
       "684182b2a6d3481c9686e34c",
@@ -35,7 +35,7 @@ function useHandleUpdateShowTimeEveryDay() {
       "684182cda6d3481c9686e54e",
       "684182dda6d3481c9686e5b7",
     ];
-    const ids3 = [
+    const idsHCM3 = [
       "684184c1a6d3481c9686f61f",
       "684184cca6d3481c9686f688",
       "684184dda6d3481c9686f6dd",
@@ -43,7 +43,7 @@ function useHandleUpdateShowTimeEveryDay() {
       "684184f6a6d3481c9686f7af",
       "68418504a6d3481c9686f818",
     ];
-    const ids4 = [
+    const idsHCM4 = [
       "684185f9a6d3481c96870d03",
       "68418604a6d3481c96870d6c",
       "68418611a6d3481c96870e77",
@@ -51,7 +51,7 @@ function useHandleUpdateShowTimeEveryDay() {
       "68418631a6d3481c96871079",
       "68418643a6d3481c96871148",
     ];
-    const ids5 = [
+    const idsHCM5 = [
       "68418711a6d3481c96871d46",
       "68418722a6d3481c96871e15",
       "68418733a6d3481c96871f20",
@@ -59,6 +59,12 @@ function useHandleUpdateShowTimeEveryDay() {
       "68418750a6d3481c96872122",
       "68418763a6d3481c968721f1",
     ];
+    const idsDN = ["68418912a6d3481c96876c70", "6841cbd6a6d3481c9687e111"];
+    const idsDN1 = ["6841db6da6d3481c96881376", "6841db7aa6d3481c968813df"];
+    const idsDN2 = ["6841dd80a6d3481c96882426", "6841dd8ca6d3481c9688248f"];
+    const idsDN3 = ["6841de2ba6d3481c9688347f", "6841de3fa6d3481c968834e8"];
+    const idsDN4 = ["6841dee4a6d3481c968844b2", "6841df04a6d3481c9688451b"];
+    const idsDN5 = ["6841dfaea6d3481c968854bf", "6841dfc6a6d3481c96885528"];
     // Tạo danh sách 7 ngày liên tiếp bắt đầu từ hôm nay
     const daysISO = [];
     const today = new Date();
@@ -70,12 +76,19 @@ function useHandleUpdateShowTimeEveryDay() {
       daysISO.push(day.toISOString());
     }
     updateShowTimeEveryday(idsDisable, today.toISOString());
-    updateShowTimeEveryday(ids, daysISO[0]);
-    updateShowTimeEveryday(ids1, daysISO[1]);
-    updateShowTimeEveryday(ids2, daysISO[2]);
-    updateShowTimeEveryday(ids3, daysISO[3]);
-    updateShowTimeEveryday(ids4, daysISO[4]);
-    updateShowTimeEveryday(ids5, daysISO[5]);
+    updateShowTimeEveryday(idsHCM, daysISO[0]);
+    updateShowTimeEveryday(idsHCM1, daysISO[1]);
+    updateShowTimeEveryday(idsHCM2, daysISO[2]);
+    updateShowTimeEveryday(idsHCM3, daysISO[3]);
+    updateShowTimeEveryday(idsHCM4, daysISO[4]);
+    updateShowTimeEveryday(idsHCM5, daysISO[5]);
+
+    updateShowTimeEveryday(idsDN, daysISO[0]);
+    updateShowTimeEveryday(idsDN1, daysISO[1]);
+    updateShowTimeEveryday(idsDN2, daysISO[2]);
+    updateShowTimeEveryday(idsDN3, daysISO[3]);
+    updateShowTimeEveryday(idsDN4, daysISO[4]);
+    updateShowTimeEveryday(idsDN5, daysISO[5]);
 
     const intervalId = setInterval(() => {
       const daysISO = [];
@@ -89,12 +102,12 @@ function useHandleUpdateShowTimeEveryDay() {
       }
 
       updateShowTimeEveryday(idsDisable, today.toISOString());
-      updateShowTimeEveryday(ids, daysISO[0]);
-      updateShowTimeEveryday(ids1, daysISO[1]);
-      updateShowTimeEveryday(ids2, daysISO[2]);
-      updateShowTimeEveryday(ids3, daysISO[3]);
-      updateShowTimeEveryday(ids4, daysISO[4]);
-      updateShowTimeEveryday(ids5, daysISO[5]);
+      updateShowTimeEveryday(idsHCM, daysISO[0]);
+      updateShowTimeEveryday(idsHCM1, daysISO[1]);
+      updateShowTimeEveryday(idsHCM2, daysISO[2]);
+      updateShowTimeEveryday(idsHCM3, daysISO[3]);
+      updateShowTimeEveryday(idsHCM4, daysISO[4]);
+      updateShowTimeEveryday(idsHCM5, daysISO[5]);
     }, 2 * 60 * 60 * 1000);
 
     return () => clearInterval(intervalId);
